57327bec284a3a122186febb49a2dd959d5da1fa
brgd <hohhots@qq.com>
2019-04-11 21:02:36 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container"><MonInput />&nbsp;Hello</section>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {}
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
