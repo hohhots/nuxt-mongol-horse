4f2dac353c7d5b5b9d4cbb43ab60df313a288e06
brgd <hohhots@qq.com>
2019-04-19 01:55:18 +0800

    fix problem in firefox

--- components/mongol/MonBody.vue
+++ components/mongol/MonBody.vue
@@ -23,19 +23,29 @@ export default {
   methods: {
     setScrollBarHeight() {
       if (!this.isMobile()) {
-        const html = document.body.innerHTML
-        document.body.innerHTML = ''
+        const html = document.documentElement
+        const body = document.body
+
+        const innerHtml = body.innerHTML
+        body.innerHTML = ''
 
         const div = document.createElement('div')
         div.style.height = '100vh'
         div.style.width = '110vw'
         document.body.appendChild(div)
         window.scrollTo(0, div.scrollHeight)
-        this.scrollBarHeight = Math.ceil(window.pageYOffset)
-        console.log(window.pageYOffset)
+        this.scrollBarHeight =
+          Math.ceil(window.pageYOffset) -
+          this.getComputedStyle(body, 'margin-top') -
+          this.getComputedStyle(body, 'margin-bottom') -
+          this.getComputedStyle(html, 'margin-top')
+        if (this.hasMarginBottom()) {
+          this.scrollBarHeight -= this.getComputedStyle(html, 'margin-bottom')
+        }
+        console.log(this.scrollBarHeight)
         document.body.removeChild(div)
 
-        document.body.innerHTML = html
+        document.body.innerHTML = innerHtml
       }
     },
     isMobile() {
@@ -47,6 +57,26 @@ export default {
       const isIpad = ua.toLowerCase().indexOf('ipad') !== -1
       const isAndroid = ua.toLowerCase().indexOf('android') !== -1
       return isIphone || isIpad || isAndroid
+    },
+    hasMarginBottom() {
+      // in firefox margin-bottom didn't work
+      const ua = navigator.userAgent
+      let has = true
+      if (ua.toLowerCase().indexOf('firefox') !== -1) {
+        has = false
+      }
+
+      return has
+    },
+    getComputedStyle(el, property) {
+      const p = window.getComputedStyle(el, null).getPropertyValue(property)
+      if (p.indexOf('px') > 0) {
+        return this.getDimensionNumber(p)
+      }
+      return p
+    },
+    getDimensionNumber(dimension) {
+      return parseInt(dimension.replace('px', ''))
     }
   }
 }
