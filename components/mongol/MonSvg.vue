398e0547c4516f34b7b6edcd208eaf81f550916a
brgd <hohhots@qq.com>
2019-05-05 19:14:45 +0800

    add MonSvg component

--- components/mongol/MonSvg.vue
+++ components/mongol/MonSvg.vue
@@ -1,16 +1,45 @@
 <template>
-  <!-- <div ref="container"> -->
-  <svg ref="svg" xmlns="http://www.w3.org/2000/svg">
-    <slot></slot>
-  </svg>
-  <!-- </div> -->
+  <div ref="container" :style="{ height: height + 'px', width: width + 'px' }">
+    <svg ref="svg" v-bind="$attrs">
+      <slot></slot>
+    </svg>
+  </div>
 </template>
 
 <script>
-export default {}
+import util from '@/util/util.js'
+
+export default {
+  inheritAttrs: false,
+  data() {
+    return {
+      width: 0,
+      height: 0
+    }
+  },
+  mounted() {
+    this._initState()
+  },
+  methods: {
+    _initState() {
+      this.height =
+        this.$refs.svg.viewBox.baseVal.width ||
+        util.getComputedStyle(this.$refs.svg, 'width')
+      this.width =
+        this.$refs.svg.viewBox.baseVal.height ||
+        util.getComputedStyle(this.$refs.svg, 'height')
+      console.log(this.height, ' - dddddddd - ', this.width)
+    }
+  }
+}
 </script>
 
 <style scoped>
+div {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+}
 svg {
   -webkit-transform-origin: left top;
   -ms-transform-origin: left top;
