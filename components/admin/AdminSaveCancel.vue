d4aa59d19326d67e981af2926791ce0e3fbdadc3
brgd <hohhots@qq.com>
2019-08-16 08:40:55 +0800

    display new page at last page

--- components/admin/AdminSaveCancel.vue
+++ components/admin/AdminSaveCancel.vue
@@ -3,24 +3,40 @@
     <div class="sava-cancel">
       <mon-button type="submit" width="70px">{{ monText.save }}</mon-button>
 
-      <mon-button type="button" width="70px" @click="$emit('cancel')">
-        {{ monText.cancel }}
-      </mon-button>
+      <mon-button type="button" width="70px" @click="$emit('cancel')">{{ monText.cancel }}</mon-button>
     </div>
 
-    <div v-if="editExistingPage" class="new-page">
-      <mon-button type="button" width="70px" @click="$emit('newPage')">{{
+    <div v-if="isLastPage()" class="new-page">
+      <mon-button type="button" width="70px" @click="$emit('newPage')">
+        {{
         monText.newPage
-      }}</mon-button>
+        }}
+      </mon-button>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import globalVariables from '@/mixins/globalVariables.js'
 
 export default {
-  mixins: [globalVariables]
+  mixins: [globalVariables],
+  computed: {
+    ...mapState({
+      book: state => state.books.book
+    })
+  },
+  beforeMount() {
+    console.log(this.book)
+  },
+  methods: {
+    isLastPage() {
+      const pageId = parseInt(this.$route.params.pageid) || 0
+      return pageId === this.book.pages.length
+    }
+  }
 }
 </script>
 
